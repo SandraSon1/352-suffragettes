4d82065a0cf762b01bda569df4b5457eb483c6c5
SandraSon1 <sandra.thi.son@gmail.com>
2018-04-23 23:37:02 +1200

    retrieve data

--- scripts/main.js
+++ scripts/main.js
@@ -10,68 +10,77 @@
   firebase.initializeApp(config);
 
 
-// //Reference messages collection
-// var messagesRef = firebase.database().ref('messages');
+
+
+//Reference messages collection
+var messagesRef = firebase.database().ref('messages');
 
 
 
 
-// //listen for form submit
-// document.getElementById('serial').addEventListener('submit', submitForm);
+//listen for form submit
+document.getElementById('serial').addEventListener('submit', submitForm);
 
-// //submit form
-// function submitForm(e){
-// 	e.preventDefault();
+//submit form
+function submitForm(e){
+	e.preventDefault();
 
-// 	//Get values
-// 	var serialNumber = getInputVal('serialNumber');
+	//Get values
+	var serialNumber = getInputVal('serialNumber');
+  
+	//save message
+	saveMessage(serialNumber);
 
-// 	//save message
-// 	saveMessage(serialNumber);
+	console.log(serialNumber);
 
-// 	console.log(serialNumber);
-// }
+  
+}
 
-// //function to get get form values
-// function getInputVal(id){
-// 	return document.getElementById(id).value;
-// }
+//function to get get form values
+function getInputVal(id){
+	return document.getElementById(id).value;
+}
 
 
-// //save message to firebase
-// function saveMessage(serialNumber){
-// 	var newMessageRef =  messagesRef.push();
-// 	newMessageRef.set({
-// 		serialNumber: serialNumber
-// 	});
-// }
+//save message to firebase
+function saveMessage(serialNumber){
+	var newMessageRef =  messagesRef.push();
+	newMessageRef.set({
+		serialNumber: serialNumber
+	});
 
+
+
+
+
+//reference to database .child reference the child
 var rootRef = firebase.database().ref();
-var masterRef = rootRef.child("messages");
-
-
-masterRef.orderByChild("number").equalTo(1).on("child_added", function(snapshot) {
+var masterRef = rootRef.child("SUFPART1");
+var listnumber = 1;
+//retrieves data 
+masterRef.orderByChild("NUMBER").equalTo(listnumber).on("child_added", function(snapshot) {
   var newPost = snapshot.val();
-  console.log(newPost.firstname);
+  console.log(newPost.FIRST_NAME);
 
-  document.getElementById("text2").innerHTML = "Name: " + newPost.firstname + " " +newPost.surname + "<br>City: " + newPost.locality; 
+  document.getElementById("text2").innerHTML = "Name: " + newPost.TTL +" "+ newPost.FIRST_NAME + " " +newPost.SURNAME+ "<br>Locality: " + newPost.LOCALITY + "<br>Town/Suburb: " + newPost.TOWN_SUBUR; 
 });
 
+}
 
 
 
-masterRef.on("child_added", function(snapshot) {
-  var newPost = snapshot.val();
+// masterRef.on("child_added", function(snapshot) {
+//   var newPost = snapshot.val();
 
-  // console.log("Name: " + newPost.firstname);
-  //     console.log("Name: " + newPost.surname);
-  //       console.log("Name: " + newPost.locality);
-document.getElementById("text").innerHTML = "Name: " + newPost.firstname + " " +newPost.surname + "<br>City: " + newPost.locality; 
-},
+//   // console.log("Name: " + newPost.firstname);
+//   //     console.log("Name: " + newPost.surname);
+//   //       console.log("Name: " + newPost.locality);
+// document.getElementById("text").innerHTML = "Name: " + newPost.firstname + " " +newPost.surname + "<br>City: " + newPost.locality; 
+// },
 
-  function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
+//   function (errorObject) {
+//   console.log("The read failed: " + errorObject.code);
+// });
 
 
 
